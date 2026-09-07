a8bec63dfe7d725e9eafab5937864f69528e0f62
king8899vip-a11y <king8899vip@gmail.com>
2026-09-07 12:18:32 +0800

    Add history year selector

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='marksix-analyzer-v1.2.0';
+const CACHE='marksix-analyzer-v1.3.0';
 const SHELL=['./','./index.html','./styles.css','./app.js','./config.js','./data-service.js','./manifest.webmanifest','./data/snapshot.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
@@ -9,3 +9,4 @@ self.addEventListener('fetch',e=>{
   }
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
+
