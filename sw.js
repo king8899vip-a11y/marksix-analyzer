436d4fbe2164e3229c133c584e4af82d7ff4d0ba
king8899vip-a11y <king8899vip@gmail.com>
2026-09-12 22:18:45 +0800

    Refresh PWA cache for backtest upgrade

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='marksix-analyzer-v1.7.0';
-const SHELL=['./','./index.html','./styles.css?v=1.7.0','./app.js?v=1.7.0','./config.js?v=1.7.0','./data-service.js?v=1.7.0','./manifest.webmanifest'];
+const CACHE='marksix-analyzer-v1.9.0';
+const SHELL=['./','./index.html','./styles.css?v=1.7.0','./app.js?v=1.9.0','./config.js?v=1.7.0','./data-service.js?v=1.7.0','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
